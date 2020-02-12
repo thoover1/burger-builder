31ab67cf0bb0118394b738de39f504321b9cbf1c
thoover1 <thoover1@uab.edu>
2020-02-12 15:29:20 -0600

    185 only update ordersummary component if modal is toggled

--- src/Components/UI/Modal/Modal.js
+++ src/Components/UI/Modal/Modal.js
@@ -1,21 +1,28 @@
-import React from "react";
+import React, { Component } from "react";
 import Aux from "../../../hoc/Auxillary";
 import Backdrop from "../Backdrop/Backdrop";
 import "./Modal.css";
 
-const modal = props => (
-  <Aux>
-    <Backdrop show={props.show} click={props.modalClosed} />
-    <div
-      className="Modal"
-      style={{
-        transform: props.show ? "translateY(0)" : "translateY(-100vh)",
-        opacity: props.show ? "1" : "0"
-      }}
-    >
-      {props.children}
-    </div>
-  </Aux>
-);
+class Modal extends Component {
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextProps.show !== this.props.show;
+  }
+  render() {
+    return (
+      <Aux>
+        <Backdrop show={this.props.show} click={this.props.modalClosed} />
+        <div
+          className="Modal"
+          style={{
+            transform: this.props.show ? "translateY(0)" : "translateY(-100vh)",
+            opacity: this.props.show ? "1" : "0"
+          }}
+        >
+          {this.props.children}
+        </div>
+      </Aux>
+    );
+  }
+}
 
-export default modal;
+export default Modal;
